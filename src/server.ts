ed32a76af74f4f3b1b6c4ea4ce424fee4a7528da
Gulikovskiy <ivan_moroz@outlook.com>
2026-07-08 13:19:18 +0300

    fix: check actions

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use("/tasks", authenticate);
 
 app.get("/health", (req: Request, res: Response) => {
-  res.json({ status: "ok-test2" });
+  res.json({ status: "ok-test4" });
 });
 
 app.post("/auth/register", async (req: Request, res: Response, next: NextFunction) => {
